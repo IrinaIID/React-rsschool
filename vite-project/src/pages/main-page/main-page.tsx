eac8f560a159e0652796c79fab3b40b71df38a5f
Iryna Ishchanka <28myhome@gmail.com>
2023-03-16 18:05:05 +0300

    feat: add footer

--- vite-project/src/pages/main-page/main-page.tsx
+++ vite-project/src/pages/main-page/main-page.tsx
@@ -1,12 +1,15 @@
 import Header from '../../components/heder/header';
 import React from 'react';
-
+import Footer from '../../components/footer/footer';
+import './main-page.scss'
 
 export default class MainPage extends React.Component {
   render() {
     return (
       <div className="main-page-all">
         <Header />
+        <div className="main">dgfdsgfs</div>
+        <Footer />
       </div>
     );
   }
