5fa4ca6280ebe2488eff99ed08126066cdbcf76e
Iryna Ishchanka <28myhome@gmail.com>
2023-03-29 00:30:38 +0300

    feat: add some tests

--- vite-project/src/components/form/create-form.test.tsx
+++ vite-project/src/components/form/create-form.test.tsx
@@ -6,6 +6,7 @@ describe('CreateForm test', () => {
   test('render test of CreateForm', () => {
     render(<CreateForm />);
     expect(screen.getByRole('button')).toBeDefined();
+    expect(screen.getByText(/Name/i)).toBeDefined();
     expect(screen.getByText(/For public view/i)).toBeDefined();
   });
 });
---
+++ vite-project/src/components/visitor-card/visitor-card.test.tsx
@@ -0,0 +1,13 @@
+import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import VisitorCard from './visitor-card';
+import Img from '../../assets/img/cards/aws.webp';
+
+describe('VisitorCard test', () => {
+  test('VisitorCard', () => {
+    render(<VisitorCard name='a' date='2023' status='guest' gender='male' public='true' picture={Img} />);
+    expect(screen.getByText(/2023/i)).toBeDefined();
+    expect(screen.getByText(/guest/i)).toBeDefined();
+    expect(screen.getByRole('img')).toBeDefined();
+  });
+});
