386e3e928e7d0ed8180bd4c99d65afa163f169bd
Iryna Ishchanka <28myhome@gmail.com>
2023-04-02 21:59:13 +0300

    feat: add tests with user events

--- vite-project/src/components/form/create-form.test.tsx
+++ vite-project/src/components/form/create-form.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import CreateForm from './create-form';
+import userEvent from '@testing-library/user-event';
 
 describe('CreateForm test', () => {
   test('render test of CreateForm', () => {
@@ -9,5 +10,21 @@ describe('CreateForm test', () => {
     expect(screen.getByText(/Name/i)).toBeDefined();
     expect(screen.getByText(/For public view/i)).toBeDefined();
     expect(screen.getByText(/Status/i)).toBeDefined();
+  });
+
+  test('check input for possible to write information', async () => {
+    render(<CreateForm />);
+    const input = screen.getByRole('textbox');
+    await userEvent.type(input, 'hello');
+    expect(screen.getByDisplayValue('hello')).toBeDefined();
+  });
+
+  test('check clean input after click button', async () => {
+    render(<CreateForm />);
+    const btn = screen.getByRole('button');
+    const input = screen.getByRole('textbox');
+    await userEvent.type(input, 'hello');
+    await userEvent.click(btn);
+    expect(screen.getByDisplayValue('')).toBeDefined();
   });
 });
--- vite-project/src/components/search-form/search-form.test.tsx
+++ vite-project/src/components/search-form/search-form.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import SearchForm from './search-form';
+import userEvent from '@testing-library/user-event';
 
 describe('Search-form test', () => {
   test('should contain button', () => {
@@ -8,5 +9,14 @@ describe('Search-form test', () => {
     expect(screen.getByRole('button')).toBeDefined();
     expect(screen.getAllByRole('button')).toHaveLength(1);
     expect(screen.getByRole('img')).toBeDefined();
+  });
+
+  test('check clean input after click button', async () => {
+    render(<SearchForm />);
+    const btn = screen.getByRole('button');
+    const input = screen.getByRole('textbox');
+    await userEvent.type(input, 'hello');
+    await userEvent.click(btn);
+    expect(screen.getByDisplayValue('')).toBeDefined();
   });
 });
