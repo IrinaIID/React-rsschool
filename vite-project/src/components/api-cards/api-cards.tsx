7ef0f3f9ce61c03a780dc4c573a7b14c1c6700a6
Iryna Ishchanka <28myhome@gmail.com>
2023-04-08 14:00:41 +0300

    feat: add fetch data from api

--- vite-project/src/components/api-cards/api-cards.tsx
+++ vite-project/src/components/api-cards/api-cards.tsx
@@ -1,9 +1,26 @@
+import Card from '../../components/card/card';
 import './api-cards.scss';
 
+interface ApiCard {
+  picture: string
+  name: string
+  species: string
+}
 
-export default function ApiCards() {
+export default function ApiCards(cardsInfo: ApiCard) {
+
   return (
-    <div className="api-cards-all">
+    <div className="card-all">
+      <div className="img-card-block">
+        <img src={cardsInfo.picture} alt="card" className="img-item" />
+      </div>
+      <div className="description-card-block">
+        <div className="description-info">
+          <p className="p-info">{`name: ${cardsInfo.name}`}</p>
+          <p className="p-info">{`species: ${cardsInfo.species}`}</p>
+        </div>
+        <button className="btn-card">choose</button>
+      </div>
     </div>
-  );
+  )
 }
--- vite-project/src/pages/main-page/main-page.tsx
+++ vite-project/src/pages/main-page/main-page.tsx
@@ -2,10 +2,44 @@ import Header from '../../components/heder/header';
 import Footer from '../../components/footer/footer';
 import './main-page.scss';
 import SearchForm from '../../components/search-form/search-form';
-import Card from '../../components/card/card';
-import { CARDS_INFO } from '../../data/cards-info';
+import ApiCards from '../../components/api-cards/api-cards';
+import { useEffect, useState } from 'react';
+
+const baseURL = 'https://rickandmortyapi.com/api/character';
+
+interface ApiInfo {
+  id: number
+  name: string
+  status: string
+  species: string
+  type: string
+  gender: string
+  origin: {
+    name: string
+    url: string
+  },
+  location: {
+    name: string
+    url: string
+  },
+  image: string
+  pisode: string[]
+  url: string
+  created: string
+}
 
 export default function MainPage() {
+  const [cardsInfo, setCardsInfo] = useState<ApiInfo[]>()
+
+  useEffect(() => {
+    fetch(baseURL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.results)
+      setCardsInfo(data.results)
+    })
+  },[])
+
   return (
     <div className="main-page-all">
       <Header />
@@ -14,15 +48,10 @@ export default function MainPage() {
           <SearchForm />
         </div>
         <div className="cards-block">
-          {CARDS_INFO.map((card) => {
+          {cardsInfo && cardsInfo.map(card => {
             return (
-              <Card
-                picture={card.picture}
-                name={card.name}
-                year={card.year}
-                key={card.id.toString()}
-              />
-            );
+              <ApiCards picture={card.image} name={card.name} species={card.species} key={card.id}/>
+            )
           })}
         </div>
       </div>
